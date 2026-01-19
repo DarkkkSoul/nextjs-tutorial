8479ceb6b367c752419052e0d9b4132373c48614
Dark Soul <maheshh.kumar1508@gmail.com>
2026-01-19 11:49:17 +0530

    loading

--- hello-world/src/app/(auth)/signin/page.tsx
+++ hello-world/src/app/(auth)/signin/page.tsx
@@ -1,3 +1,8 @@
-export default function SignIn(){
-    return <h1>Sign-In function</h1>
+export default async function SignIn() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Intentional Delay...");
+    }, 500);
+  });
+  return <h1>Sign-In function</h1>;
 }
--- hello-world/src/app/(auth)/signup/page.tsx
+++ hello-world/src/app/(auth)/signup/page.tsx
@@ -1,3 +1,8 @@
-export default function SignOut(){
-    return <h1>Sign-up function</h1>
+export default async function SignOut() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Intentional Delay...");
+    }, 500);
+  });
+  return <h1>Sign-up function</h1>;
 }
