4c21aae78fe1bb64497f60a1656cd5427bf0bb21
Dark Soul <maheshh.kumar1508@gmail.com>
2026-01-27 17:54:44 +0530

    post handler

--- route-handler/src/app/comments/route.ts
+++ route-handler/src/app/comments/route.ts
@@ -3,3 +3,17 @@ import { commentsArray } from "./comments";
 export async function GET() {
     return Response.json(commentsArray);
 }
+
+export async function POST(request: Request) {
+    const comment = await request.json();
+    const newComment = {
+        id: commentsArray.length + 1,
+        comment: comment.comment
+    }
+    commentsArray.push(newComment);
+
+    return new Response(JSON.stringify(newComment), {
+        headers: { "Content-Type": "application/json" },
+        status: 201
+    });
+}
