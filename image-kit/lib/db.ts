8912285b960e0c06152f363d6d04e5d3b064391b
Mahesh <maheshh.kumar1508@gmail.com>
2026-04-05 20:46:57 +0530

    db connection

---
+++ image-kit/lib/db.ts
@@ -0,0 +1,30 @@
+import { mongoose } from "mongoose"
+
+const DB_URI = process.env.DB_URI!
+
+if (!DB_URI) {
+    throw new Error("Define database uri in env variable")
+}
+
+let cached = global.mongoose
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null }
+}
+
+export const connectToDB = async () => {
+    if (cached.conn) return cached.conn
+
+    if (!cached.promise) {
+        mongoose.connect(DB_URI)
+            .then(() => mongoose.connection)
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (error) {
+        throw error;
+    }
+
+    return cached.conn;
+}
